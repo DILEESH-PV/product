3fedf2d46be49c0dae05127c6d9c6749eddf5f24
DILEESH-PV <dileeshpv1998@gmail.com>
2022-11-03 22:57:28 +0530

    second commit

--- product.py
+++ product.py
@@ -9,20 +9,19 @@ except mysql.connector.Error as e:
 mycursor=mydb.cursor()
 data=requests.get("https://dummyjson.com/products").text
 data_info=json.loads(data)
-user_list=[]
-for i in data_info:
-    user_list.append(i)
-test=user_list[0]
-pre=data_info[test]
-for i in pre:
-    #user_list.append([i["title"],i["description"],i["price"]])
-    pri=str(i['price'])
-    dis=str(i['discountPercentage'])
-    rat=str(i['rating'])
-    stoc=str(i['stock'])
-    sql='INSERT INTO `product`(`title`, `description`, `price`, `discount`, `rating`, `stock`, `brand`, `category`) VALUES ("'+i['title']+'","'+i['description']+'","'+pri+'","'+dis+'","'+rat+'","'+stoc+'","'+i['brand']+'","'+i['category']+'")'
-    #sql="INSERT INTO `product`(`title`, `description`, `price`, `discount`, `rating`, `stock`, `brand`, `category`) VALUES ('"+i['title']+"','"+i['description']+"','"+pri+"','"+dis+"','"+rat+"','"+stoc+"','"+i['brand']+"','"+i['category']+"')"
-    mycursor.execute(sql)
-    mydb.commit()
-    print("Data inserted successfully",i["title"])
-    #print(i['title'],"\n",i['description'],"\n",i['price'],"\n",i['discountPercentage'],"\n",i['rating'],"\n",i['stock'],"\n",i['brand'],"\n",i['category'])
+
+for i in data_info.values():
+    if(isinstance(i or j,int)):
+        break
+    for j in i:
+        #user_list.append([i["title"],i["description"],i["price"]])
+        pri=str(j['price'])
+        dis=str(j['discountPercentage'])
+        rat=str(j['rating'])
+        stoc=str(j['stock'])
+        sql='INSERT INTO `product`(`title`, `description`, `price`, `discount`, `rating`, `stock`, `brand`, `category`) VALUES ("'+j['title']+'","'+j['description']+'","'+pri+'","'+dis+'","'+rat+'","'+stoc+'","'+j['brand']+'","'+j['category']+'")'
+        #sql="INSERT INTO `product`(`title`, `description`, `price`, `discount`, `rating`, `stock`, `brand`, `category`) VALUES ('"+i['title']+"','"+i['description']+"','"+pri+"','"+dis+"','"+rat+"','"+stoc+"','"+i['brand']+"','"+i['category']+"')"
+        mycursor.execute(sql)
+        mydb.commit()
+        print("Data inserted successfully",j['title'])
+        #print(i['title'],"\n",i['description'],"\n",i['price'],"\n",i['discountPercentage'],"\n",i['rating'],"\n",i['stock'],"\n",i['brand'],"\n",i['category'])
