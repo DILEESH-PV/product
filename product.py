626842404f7b49176075bbe075edf6e038a53a35
DILEESH-PV <dileeshpv1998@gmail.com>
2022-11-04 09:07:05 +0530

    simplified

--- product.py
+++ product.py
@@ -9,19 +9,15 @@ except mysql.connector.Error as e:
 mycursor=mydb.cursor()
 data=requests.get("https://dummyjson.com/products").text
 data_info=json.loads(data)
-
-for i in data_info.values():
-    if(isinstance(i or j,int)):
-        break
-    for j in i:
-        #user_list.append([i["title"],i["description"],i["price"]])
-        pri=str(j['price'])
-        dis=str(j['discountPercentage'])
-        rat=str(j['rating'])
-        stoc=str(j['stock'])
-        sql='INSERT INTO `product`(`title`, `description`, `price`, `discount`, `rating`, `stock`, `brand`, `category`) VALUES ("'+j['title']+'","'+j['description']+'","'+pri+'","'+dis+'","'+rat+'","'+stoc+'","'+j['brand']+'","'+j['category']+'")'
+for j in data_info['products']:
+    #print(str(j['title']))
+    pri=str(j['price'])
+    dis=str(j['discountPercentage'])
+    rat=str(j['rating'])
+    stoc=str(j['stock'])
+    sql='INSERT INTO `product`(`title`, `description`, `price`, `discount`, `rating`, `stock`, `brand`, `category`) VALUES ("'+j['title']+'","'+j['description']+'","'+pri+'","'+dis+'","'+rat+'","'+stoc+'","'+j['brand']+'","'+j['category']+'")'
         #sql="INSERT INTO `product`(`title`, `description`, `price`, `discount`, `rating`, `stock`, `brand`, `category`) VALUES ('"+i['title']+"','"+i['description']+"','"+pri+"','"+dis+"','"+rat+"','"+stoc+"','"+i['brand']+"','"+i['category']+"')"
-        mycursor.execute(sql)
-        mydb.commit()
-        print("Data inserted successfully",j['title'])
-        #print(i['title'],"\n",i['description'],"\n",i['price'],"\n",i['discountPercentage'],"\n",i['rating'],"\n",i['stock'],"\n",i['brand'],"\n",i['category'])
+    mycursor.execute(sql)
+    mydb.commit()
+    print("Data inserted successfully",j['title'])
+        
